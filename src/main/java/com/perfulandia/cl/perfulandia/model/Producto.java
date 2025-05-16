d03f40cfcae3abaa9566bf317799a751753d587a
xXGONZALOx <gonzaloberrios148@gmail.com>
2025-05-16 17:14:04 -0400

    nuevo

---
+++ src/main/java/com/perfulandia/cl/perfulandia/model/Producto.java
@@ -0,0 +1,21 @@
+package com.perfulandia.cl.perfulandia.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+public class Producto {
+
+    
+
+
+}
